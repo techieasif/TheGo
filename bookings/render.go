36e7127773b0488bf9c723534cabdb38e99c3b84
Mohd Asif <techieasif@gmail.com>
2023-02-11 11:42:01 +0530

    the templates

--- bookings/render.go
+++ bookings/render.go
@@ -10,6 +10,6 @@ func renderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Println("error parsing templates:", err)
+		fmt.Println("error parsing the templates:", err)
 	}
 }
